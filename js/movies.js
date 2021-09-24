2fefdbd8fc1e59420f2223206769906412f8be94
gao-laoshi <90668075+gao-laoshi@users.noreply.github.com>
2021-09-24 02:19:49 -0500

    Add files via upload

---
+++ js/movies.js
@@ -0,0 +1,53 @@
+var list = [
+	{
+		id: 0,
+		name : "中国医生",
+		image : "https://i.loli.net/2021/09/24/qJnSetOrpbFGyzP.jpg",
+		path: "https://jx.mmkv.cn/m3u8.php?url=https://cdn.oss-cn-hangzhou.myqcloud.com.xuetuiguang.cn/m3u8video/zgys.m3u8"
+	},{
+		id: 1,
+		name : "唐人街探案3",
+		image : "https://i.loli.net/2021/09/24/g3WRxB7XaniomNT.jpg",
+		path: "https://www.cuan.la/m3u8.php?url=https://www.iqiyi.com/v_19rrcuh1jw.html?vfm=2008_aldbd"
+	},{
+		id: 2,
+		name : "你好，李焕英",
+		image : "https://i.loli.net/2021/09/24/VNFAyCHXbwgznQJ.jpg",
+		path: "https://jx.mmkv.cn/m3u8.php?url=https://cloud.renrenmi.cc:2323/video/1623997143/14232.m3u8"
+	},{
+		id: 3,
+		name : "日不落酒店",
+		image : "https://i.loli.net/2021/09/24/g7ZKnykaQCPAJu8.png",
+		path: "https://jx.mmkv.cn/m3u8.php?url=https://data.hfyrw.com/api/cache/datayouku/XNTE1MzU5MTEzNg.m3u8"
+	},{
+		id: 4,
+		name : "总是有爱在隔离",
+		image : "https://i.loli.net/2021/09/24/dFRE8LlBTqU5ASN.jpg",
+		path: "https://jx.mmkv.cn/m3u8.php?url=https://ali-video.h2vcdn.xyz/vcdn/toutiao/3263717988-3e1a3c1931387ae1a3e8a331e1d36ed8/videos/movie/202x/%E6%80%BB%E6%98%AF%E6%9C%89%E7%88%B1%E5%9C%A8%E9%9A%94%E7%A6%BB%E5%9B%BD%E8%AF%AD.m3u8"
+	},{
+		id: 5,
+		name : "电锯惊魂9：漩涡",
+		image : "https://i.loli.net/2021/09/24/HpnYZOulW1wQDjX.jpg",
+		path: "https://jx.mmkv.cn/m3u8.php?url=https://v7.dious.cc/20210601/KIG4PLhm/1000kb/hls/index.m3u8"
+	},{
+		id: 6,
+		name : "扫黑·决战",
+		image : "https://i.loli.net/2021/09/24/5H2UpgNbKWnR1je.jpg",
+		path: "https://jx.mmkv.cn/m3u8.php?url=https://data.hfyrw.com/api/cache/datayouku/XNTE2MDQ1NDkyOA.m3u8"
+	},{
+		id: 7,
+		name : "温暖的抱抱",
+		image : "https://i.loli.net/2021/09/24/QWKRsvB54tN6Znm.jpg",
+		path: "https://jx.mmkv.cn/m3u8.php?url=https://ali-video.h2vcdn.xyz/vcdn/qq/3263721108-c2e69567ff861f427b47ed1945b90cb5/videos/movie/202x/%E6%B8%A9%E6%9A%96%E7%9A%84%E6%8A%B1%E6%8A%B1.m3u8"
+	},{
+		id: 8,
+		name : "刺杀小说家",
+		image : "https://i.loli.net/2021/09/24/q3cvjCdAalkEFW9.jpg",
+		path: "https://jx.mmkv.cn/m3u8.php?url=https://sf3-dycdn-tos.pstatp.com/obj/tos-cn-v-0000/8dfd490128b64cffbf10f6a50c6b91bc"
+	},{
+		id: 9,
+		name : "第十一回",
+		image : "https://i.loli.net/2021/09/24/a9SgzfIMH5s7cWk.jpg",
+		path: "https://jx.ihuikr.com/player/?url=https://www.iqiyi.com/v_19rsiogugk.html"
+	}
+];
