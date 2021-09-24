9dd79c812bc54be813f8ed31d303fdad2b569bb4
gao-laoshi <90668075+gao-laoshi@users.noreply.github.com>
2021-09-24 18:12:00 +0800

    Update movies.js

--- js/movies.js
+++ js/movies.js
@@ -2,52 +2,52 @@ var moviesList = [
 	{
 		id: 0,
 		name : "中国医生",
-		image : "https://i.loli.net/2021/09/24/qJnSetOrpbFGyzP.jpg",
+		image : "https://pic1.iqiyipic.com/image/20210921/73/e3/v_160116650_m_601_m7_260_360.jpg",
 		path: "https://jx.mmkv.cn/m3u8.php?url=https://cdn.oss-cn-hangzhou.myqcloud.com.xuetuiguang.cn/m3u8video/zgys.m3u8"
 	},{
 		id: 1,
 		name : "唐人街探案3",
-		image : "https://i.loli.net/2021/09/24/g3WRxB7XaniomNT.jpg",
+		image : "https://pic6.iqiyipic.com/image/20210514/ee/15/v_115684213_m_601_m21_260_360.jpg",
 		path: "https://www.cuan.la/m3u8.php?url=https://www.iqiyi.com/v_19rrcuh1jw.html?vfm=2008_aldbd"
 	},{
 		id: 2,
 		name : "你好，李焕英",
-		image : "https://i.loli.net/2021/09/24/VNFAyCHXbwgznQJ.jpg",
+		image : "https://pic0.iqiyipic.com/image/20210205/06/5a/v_154362925_m_601_m3_260_360.jpg",
 		path: "https://jx.mmkv.cn/m3u8.php?url=https://cloud.renrenmi.cc:2323/video/1623997143/14232.m3u8"
 	},{
 		id: 3,
 		name : "日不落酒店",
-		image : "https://i.loli.net/2021/09/24/g7ZKnykaQCPAJu8.png",
+		image : "https://pic2.iqiyipic.com/image/20210915/69/90/v_119141024_m_601_m12_260_360.jpg",
 		path: "https://jx.mmkv.cn/m3u8.php?url=https://data.hfyrw.com/api/cache/datayouku/XNTE1MzU5MTEzNg.m3u8"
 	},{
 		id: 4,
 		name : "总是有爱在隔离",
-		image : "https://i.loli.net/2021/09/24/dFRE8LlBTqU5ASN.jpg",
+		image : "https://pic8.iqiyipic.com/image/20210915/a2/23/v_157184214_m_601_m7_260_360.jpg",
 		path: "https://jx.mmkv.cn/m3u8.php?url=https://ali-video.h2vcdn.xyz/vcdn/toutiao/3263717988-3e1a3c1931387ae1a3e8a331e1d36ed8/videos/movie/202x/%E6%80%BB%E6%98%AF%E6%9C%89%E7%88%B1%E5%9C%A8%E9%9A%94%E7%A6%BB%E5%9B%BD%E8%AF%AD.m3u8"
 	},{
 		id: 5,
 		name : "电锯惊魂9：漩涡",
-		image : "https://i.loli.net/2021/09/24/HpnYZOulW1wQDjX.jpg",
+		image : "https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2637809736.webp",
 		path: "https://jx.mmkv.cn/m3u8.php?url=https://v7.dious.cc/20210601/KIG4PLhm/1000kb/hls/index.m3u8"
 	},{
 		id: 6,
 		name : "扫黑·决战",
-		image : "https://i.loli.net/2021/09/24/5H2UpgNbKWnR1je.jpg",
+		image : "https://pic9.iqiyipic.com/image/20210915/06/a7/v_151351479_m_601_m13_260_360.jpg",
 		path: "https://jx.mmkv.cn/m3u8.php?url=https://data.hfyrw.com/api/cache/datayouku/XNTE2MDQ1NDkyOA.m3u8"
 	},{
 		id: 7,
 		name : "温暖的抱抱",
-		image : "https://i.loli.net/2021/09/24/QWKRsvB54tN6Znm.jpg",
+		image : "https://pic3.iqiyipic.com/image/20201217/b4/93/v_152475923_m_601_m2_260_360.jpg",
 		path: "https://jx.mmkv.cn/m3u8.php?url=https://ali-video.h2vcdn.xyz/vcdn/qq/3263721108-c2e69567ff861f427b47ed1945b90cb5/videos/movie/202x/%E6%B8%A9%E6%9A%96%E7%9A%84%E6%8A%B1%E6%8A%B1.m3u8"
 	},{
 		id: 8,
 		name : "刺杀小说家",
-		image : "https://i.loli.net/2021/09/24/q3cvjCdAalkEFW9.jpg",
+		image : "https://pic3.iqiyipic.com/image/20210414/a9/dd/v_122200379_m_601_m20_260_360.jpg",
 		path: "https://jx.mmkv.cn/m3u8.php?url=https://sf3-dycdn-tos.pstatp.com/obj/tos-cn-v-0000/8dfd490128b64cffbf10f6a50c6b91bc"
 	},{
 		id: 9,
 		name : "第十一回",
-		image : "https://i.loli.net/2021/09/24/a9SgzfIMH5s7cWk.jpg",
+		image : "https://pic7.iqiyipic.com/image/20210526/50/71/v_127763044_m_601_m9_260_360.jpg",
 		path: "https://jx.ihuikr.com/player/?url=https://www.iqiyi.com/v_19rsiogugk.html"
 	}
 ];
